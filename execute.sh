3407fd355ad33202d52ba5851fd59f3379955f21
Blair Nangle <blair.nangle@gmail.com>
2020-05-16 00:46:25 +0100

    Update reference to renamed file: execute.clj

--- execute.sh
+++ execute.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-lein exec -p src/twitter_nlp/core.clj ${1} ${2} ${3} ${4}
+lein exec -p src/twitter_nlp/execute.clj ${1} ${2} ${3} ${4}
 
 exit 0
--- execute_local.sh
+++ execute_local.sh
@@ -2,6 +2,6 @@
 
 source ./auth.config
 
-lein exec -p src/twitter_nlp/core.clj ${api_key} ${api_secret_key} ${access_token} ${access_token_secret}
+lein exec -p src/twitter_nlp/execute.clj ${api_key} ${api_secret_key} ${access_token} ${access_token_secret}
 
 exit 0
